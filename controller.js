7792d2e5b1a8df3a6478e65e9908c67f6929e44f
matthew <mattclarkeruns@gmail.com>
2017-12-22 11:24:50 -0500

    Create initialize function

--- controller.js
+++ controller.js
@@ -2,5 +2,11 @@ console.log("controller successfully loaded");
 
 Minesweeper.Controller = function(node) {
   return {
+    initialize: function() {
+      console.log("initialization successful");
+    },
   };
 };
+//temp for debugging
+var controller = Minesweeper.Controller($(".minesweeper")[0]);
+controller.initialize();
