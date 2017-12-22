ec9f2ed9ee1244c76735b557d9cbcf655667b6e9
matthew <mattclarkeruns@gmail.com>
2017-12-22 11:25:21 -0500

    Create populateGameRows function

--- controller.js
+++ controller.js
@@ -1,9 +1,40 @@
 console.log("controller successfully loaded");
 
 Minesweeper.Controller = function(node) {
+  // Private
+  var minesweeper = node;
+  var gameTileTemplate = $(".gameTileTemplate");
+  var gameRowTemplate = $(".gameRowTemplate");
+  var gameGrid = $(".gameGrid");
+  // Public
   return {
     initialize: function() {
       console.log("initialization successful");
+    },
+    populateGameRows: function(numRows,numCol) {
+      for(var i=0; i<numRows; i++) {
+        var newRow = this._getFormattedGameRow();
+        for(var j=0; j<numCol; j++) {
+          var newTile = this._getFormattedGameTile(i,j);
+          newRow.append(newTile);
+        };
+        gameGrid.append(newRow);
+      };
+    },
+    _getFormattedGameRow: function() {
+      var newRow = gameRowTemplate.clone();
+      newRow.removeAttr("hidden");
+      newRow.removeClass("gameRowTemplate").addClass("gameRow");
+      newRow.empty();
+      return newRow;
+    },
+    _getFormattedGameTile: function(xcoord,ycoord) {
+      var newTile = gameTileTemplate.clone();
+      newTile.removeAttr("hidden");
+      newTile.removeClass("gameTileTemplate").addClass("gameTile");
+      newTile.attr("data-xcoord",xcoord);
+      newTile.attr("data-ycoord",ycoord);
+      return newTile;
     },
   };
 };
